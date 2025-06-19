90cf896ffb520f781de5d5c6d8c6d77c794324cc
artur.ti.solution <artur.ti.solution@gmail.com>
2025-06-19 17:05:15 -0300

    adicionando registro de ocorrencias

--- src/app/demo/ocorrencias/ocorrencia-form/ocorrencia-form.ts
+++ src/app/demo/ocorrencias/ocorrencia-form/ocorrencia-form.ts
@@ -4,9 +4,22 @@ import { DOC_ORIENTATION, NgxImageCompressService } from 'ngx-image-compress';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 
 export interface Foto {
+  id?: number;
+  nome: string;
+  spotId?: number;
+  referencia?: boolean;
+  sincronizacaoS3?: boolean;
+  size?: number;
+}
 
-    id?: number;
-    nome: string;    
+export interface Ocorrencia {
+  id?: number;
+  placa: string;
+  marca?: string;
+  modelo?: string;
+  cor?: string;
+  tipoVeiculo: number;
+  foto?: Foto;
 }
 @Component({
   selector: 'app-ocorrencia-form',
@@ -15,63 +28,63 @@ export interface Foto {
   templateUrl: './ocorrencia-form.html',
   styleUrl: './ocorrencia-form.scss'
 })
-export default class OcorrenciaForm {  
-
-  imageCompress = inject(NgxImageCompressService)
-  imgResultBeforeCompress = "";
-  imgResultAfterCompress = "";
-  fotos:  Foto[] = [];
+export default class OcorrenciaForm {
+  imageCompress = inject(NgxImageCompressService);
+  imgResultBeforeCompress = '';
+  imgResultAfterCompress = '';
+  fotos: Foto[] = [];
+  ocorrencia: Ocorrencia = {
+    id: 0,
+    placa: '',
+    tipoVeiculo: -1
+  };
 
   //https://parallelum.com.br/fipe/api/v1/carros/marcas apis para obter as marcas de carro
 
   //https://parallelum.com.br/fipe/api/v1/carros/marcas/{codigoMarca}/modelos apis para obter todos os modelos de carro
 
+  addOcorrencia() {
+    // alert('Adicionando ocorrencia');
+    console.log(JSON.stringify(this.ocorrencia));
+    console.log(JSON.stringify(this.fotos));
+  }
 
-  adicionarFoto(event: any) {       
-    
+  adicionarFoto(event: any) {
     //this.msgErro='';
     for (let i = 0; i < event.target.files.length; i++) {
       const file = event.target.files[i];
       console.clear();
-      console.log(file);      
-      //if(file.size<5000000){
+      console.log(file);
+      if (file.size < 5000000) {
         const reader = new FileReader();
         reader.readAsDataURL(file);
 
-        reader.onload = () => {         
+        reader.onload = () => {
+          console.log(reader);
+          const fotoBase64 = reader.result + '';
 
-            console.log(reader);            
-            const fotoBase64 = reader.result + '';
-            
-            this.imageCompress.compressFile(fotoBase64, DOC_ORIENTATION.Default, 50, 50).then(
-              result => {
-                this.imgResultAfterCompress = result;
+          this.imageCompress.compressFile(fotoBase64, DOC_ORIENTATION.Default, 50, 50).then((result) => {
+            this.imgResultAfterCompress = result;
 
-                  const foto: Foto = {
-                    nome: this.imgResultAfterCompress,
-                  };
-                  this.fotos.push(foto);
+            const foto: Foto = {
+              nome: this.imgResultAfterCompress
+            };
+            this.fotos.push(foto);
 
-                console.warn('Size in bytes is now:', this.imageCompress.byteCount(result));
-              }
-            );           
-            
+            console.warn('Size in bytes is now:', this.imageCompress.byteCount(result));
+          });
         };
 
         reader.onerror = function (error) {
           console.log('Error: ', error);
         };
-    /*  }else{
-        this.exibirErro(1, 'Foto está acima do limite de 5MB permitida.');
-        
-      }     */ 
-    } 
+      } else {
+        alert('Foto está acima do limite de 5MB permitida.');
+      }
+    }
   }
 
-   excluirFoto(dataItem: any) {
-    
-    this.fotos = this.fotos.filter(item => item.nome !== dataItem.nome);    
+  excluirFoto(dataItem: any) {
+    this.fotos = this.fotos.filter((item) => item.nome !== dataItem.nome);
   }
-
-
 }
