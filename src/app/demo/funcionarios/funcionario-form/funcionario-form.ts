41a7d7adebda51bad5e992a558d6b2bdeea6cb64
artur.ti.solution <artur.ti.solution@gmail.com>
2025-07-01 12:03:38 -0300

    adicionando entidades de funcionario e veiculo

--- src/app/demo/funcionarios/funcionario-form/funcionario-form.ts
+++ src/app/demo/funcionarios/funcionario-form/funcionario-form.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { FuncionarioService } from '../../services/funcionario.service';
 
-export interface Funcionario {
+export interface FuncionarioModel {
   matricula: string;
   nome: string;
   cpf: string;
@@ -9,8 +12,18 @@ export interface Funcionario {
 
 @Component({
   selector: 'app-funcionario-form',
-  imports: [],
+  imports: [SharedModule, NgbDropdownModule],
   templateUrl: './funcionario-form.html',
   styleUrl: './funcionario-form.scss'
 })
-export default class FuncionarioForm {}
+export default class FuncionarioForm {
+  funcionarioService = inject(FuncionarioService);
+  funcionario: FuncionarioModel = {
+    matricula: '',
+    nome: '',
+    cpf: '',
+    telefone: ''
+  };
+
+  addFuncionario() {}
+}
--- src/app/demo/veiculos/veiculo-form/veiculo-form.ts
+++ src/app/demo/veiculos/veiculo-form/veiculo-form.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { VeiculoService } from '../../services/veiculo.service';
 
-export interface Veiculo {
+export interface VeiculoModel {
   id?: number;
   placa?: string;
   modelo?: string;
@@ -12,8 +15,23 @@ export interface Veiculo {
 
 @Component({
   selector: 'app-veiculo-form',
-  imports: [],
+  imports: [SharedModule, NgbDropdownModule],
   templateUrl: './veiculo-form.html',
   styleUrl: './veiculo-form.scss'
 })
-export default class VeiculoForm {}
+export default class VeiculoForm {
+  veiculoService = inject(VeiculoService);
+  veiculo: VeiculoModel = {
+    id: -1,
+    placa: '',
+    marca: '',
+    modelo: '',
+    matricula: '',
+    tipo_veiculo: '',
+    cor: ''
+  };
+
+  addVeiculo() {
+    console.log('veiculo ');
+  }
+}
